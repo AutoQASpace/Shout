e12102c24a4a859cf423e3800182990314529911
p.starodubkin <p.starodubkin@space307.com>
2023-04-14 23:28:39 +0300

    test2

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.7
 // Managed by ice
 
 import PackageDescription
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "Shout",
     platforms: [
-        .macOS(.v10_10)
+        .macOS(.v12)
     ],
     products: [
         .library(name: "Shout", targets: ["Shout"]),
@@ -15,8 +15,15 @@ let package = Package(
         .package(url: "https://github.com/Kitura/BlueSocket", from: "1.0.46"),
     ],
     targets: [
-        .systemLibrary(name: "CSSH", pkgConfig: "libssh2", providers: [.brew(["libssh2","openssl"])]),
-        .target(name: "Shout", dependencies: ["CSSH", "Socket"]),
-        .testTarget(name: "ShoutTests", dependencies: ["Shout"]),
+        .systemLibrary(name: "CSSH", path: "Sources/CSSH", pkgConfig: "libssh2", providers: [.brew(["libssh2","openssl"])]),
+        .target(
+            name: "Shout",
+            dependencies: [
+                "CSSH",
+                .product(name: "Socket", package: "BlueSocket"),
+            ],
+            path: "Sources/Shout"
+        ),
+        .testTarget(name: "ShoutTests", dependencies: ["Shout"], path: "Tests/ShoutTests"),
     ]
 )
